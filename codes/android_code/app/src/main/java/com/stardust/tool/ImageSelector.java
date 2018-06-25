83e0cb0872c7d6c981118f9c74dd2e83870b029d
lingshuang.kong <lingshuang.kong@cootek.cn>
2018-06-25 22:46:56 +0800

    添加图片选择类

--- codes/android_code/app/src/main/java/com/stardust/tool/ImageSelector.java
+++ codes/android_code/app/src/main/java/com/stardust/tool/ImageSelector.java
@@ -15,41 +15,41 @@ import java.io.InputStream;
 
 public class ImageSelector implements OnActivityResultDelegate {
 
-    public interface ImageSelectorCallback {
-        void onImageSelected(ImageSelector selector, InputStream path);
-    }
+     public interface ImageSelectorCallback {
+          void onImageSelected(ImageSelector selector, InputStream path);
+     }
 
-    private static final int REQUEST_CODE = 1234;
-    private Activity mActivity;
-    private ImageSelectorCallback mCallback;
+     private static final int REQUEST_CODE = 1234;
+     private Activity mActivity;
+     private ImageSelectorCallback mCallback;
 
-    public ImageSelector(Activity activity, Intermediary intermediary, ImageSelectorCallback callback) {
-        intermediary.addDelegate(REQUEST_CODE, this);
-        mActivity = activity;
-        mCallback = callback;
-    }
+     public ImageSelector(Activity activity, Intermediary intermediary, ImageSelectorCallback callback) {
+          intermediary.addDelegate(REQUEST_CODE, this);
+          mActivity = activity;
+          mCallback = callback;
+     }
 
-    public void select() {
-        mActivity.startActivityForResult(Intent.createChooser(
-                new Intent(Intent.ACTION_OPEN_DOCUMENT).setType("*/*"), mActivity.getString(R.string.text_select_image)),
-                REQUEST_CODE);
-    }
+     public void select() {
+          mActivity.startActivityForResult(Intent.createChooser(
+                    new Intent(Intent.ACTION_OPEN_DOCUMENT).setType("*/*"), mActivity.getString(R.string.text_select_image)),
+                    REQUEST_CODE);
+     }
 
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (data == null) {
-            mCallback.onImageSelected(this, null);
-            return;
-        }
-        try {
-            InputStream inputStream = mActivity.getContentResolver().openInputStream(data.getData());
-            mCallback.onImageSelected(this, inputStream);
-        } catch (FileNotFoundException e) {
-            mCallback.onImageSelected(this, null);
-        }
+     @Override
+     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+          if (data == null) {
+               mCallback.onImageSelected(this, null);
+               return;
+          }
+          try {
+               InputStream inputStream = mActivity.getContentResolver().openInputStream(data.getData());
+               mCallback.onImageSelected(this, inputStream);
+          } catch (FileNotFoundException e) {
+               mCallback.onImageSelected(this, null);
+          }
 
-    }
+     }
 
 
 }
